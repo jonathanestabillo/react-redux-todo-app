4a4e6036b05671d72dd4a60c132fec9bcf23b4db
Jonathan Estabillo <jonathan.estabillo@gmail.com>
2019-02-20 21:46:57 +0800

    Updates

---
+++ precache-manifest.3ae2b76c707e47422d27b68a00d3b540.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/react-redux-todo-app/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "ee7b96fa85d8fdb8c126409326ac2d2b",
+    "url": "/react-redux-todo-app/static/media/Roboto-Bold.ee7b96fa.ttf"
+  },
+  {
+    "revision": "8ebaa40424a706ae339f",
+    "url": "/react-redux-todo-app/static/js/runtime~main.8ebaa404.js"
+  },
+  {
+    "revision": "c33e6cacbe5e780dc32c",
+    "url": "/react-redux-todo-app/static/js/main.c33e6cac.chunk.js"
+  },
+  {
+    "revision": "f9671bf61f213243b7e0",
+    "url": "/react-redux-todo-app/static/js/2.f9671bf6.chunk.js"
+  },
+  {
+    "revision": "c33e6cacbe5e780dc32c",
+    "url": "/react-redux-todo-app/static/css/main.b166f614.chunk.css"
+  },
+  {
+    "revision": "f9671bf61f213243b7e0",
+    "url": "/react-redux-todo-app/static/css/2.fc94ea6c.chunk.css"
+  },
+  {
+    "revision": "3c0d651d9e45cf85486650e7d981ce68",
+    "url": "/react-redux-todo-app/index.html"
+  }
+];
